281f6a6de6fef6bc6b10965f146281176acf6ea5
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:18:21 +0800

    table link

---
+++ app/Views/contents/table.php
@@ -0,0 +1,43 @@
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Basic    <div>Table</div></h4>
+        <a href="<?=base_url("/table/table1")?>"  class="list-group-item ">Light</a>
+        <a href="<?=base_url("/table/table2")?>"   class="list-group-item "> Dark</a>
+        <a href="<?=base_url("/table/table3")?>"   class="list-group-item "> Primary</a>
+</div>
+
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Striped    <div>Table</div></h4>
+        <a href="<?=base_url("/table/table4")?>"   class="list-group-item">Light Striped</a>
+        <a href="<?=base_url("/table/table5")?>"   class="list-group-item">Dark Striped</a>
+        <a href="<?=base_url("/table/table6")?>"   class="list-group-item ">Primary Striped</a>
+</div>
+
+
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Hover    <div>Table</div></h4>
+        <a href="<?=base_url("/table/table7")?>"   class="list-group-item ">Light Hover</a>
+        <a href="<?=base_url("/table/table8")?>"   class="list-group-item ">Dark Hover</a>
+        <a href="<?=base_url("/table/table9")?>"   class="list-group-item ">Primary Hover</a>
+</div>
+
+
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Bordered    <div>Table</div></h4>
+        <a href="<?=base_url("/table/table10")?>"   class="list-group-item ">Light Bordered</a>
+        <a href="<?=base_url("/table/table11")?>"   class="list-group-item">Dark Bordered</a>
+        <a href="<?=base_url("/table/table12")?>"   class="list-group-item ">Primary Bordered</a>
+</div>
+
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Borderless    <div>Table</div></h4>
+        <a href="<?=base_url("/table/table13")?>"   class="list-group-item ">Light Borderless</a>
+        <a href="<?=base_url("/table/table14")?>"   class="list-group-item ">Dark Borderless</a>
+        <a href="<?=base_url("/table/table15")?>"   class="list-group-item ">Primary Borderless</a>
+</div>
+
+
+ 
+
+
+
