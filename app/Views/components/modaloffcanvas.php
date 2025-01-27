ea36f8c9a60f73bd27a2f6c740ba97cb35b2a198
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:15:15 +0800

    modal link

---
+++ app/Views/components/modaloffcanvas.php
@@ -0,0 +1,17 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"><div>Modal</div></h4>
+    <a href="<?= base_url("/modaloffcanvas/default_modal") ?>" class="list-group-item">Default Modal</a>
+    <a href="<?= base_url("/modaloffcanvas/centered_modal") ?>" class="list-group-item">Centered Modal</a>
+    <a href="<?= base_url("/modaloffcanvas/scrollable_modal") ?>" class="list-group-item">Scrollable Modal</a>
+    <a href="<?= base_url("/modaloffcanvas/static_modal") ?>" class="list-group-item">Static Backdrop Modal</a>
+    <a href="<?= base_url("/modaloffcanvas/fullscreen_modal") ?>" class="list-group-item">Fullscreen Modal</a>
+    <a href="<?= base_url("/modaloffcanvas/centered_scrollable_modal") ?>" class="list-group-item">Centered Scrollable Modal</a>
+    <a href="<?= base_url("/modaloffcanvas/large_modal") ?>" class="list-group-item">Large Modal</a>
+
+    <h4 class="d-flex justify-content-between"><div>Offcanvas</div></h4>
+    <a href="<?= base_url("/modaloffcanvas/default_offcanvas") ?>" class="list-group-item">Default Offcanvas</a>
+    <a href="<?= base_url("/modaloffcanvas/right_offcanvas") ?>" class="list-group-item">Right Offcanvas</a>
+    <a href="<?= base_url("/modaloffcanvas/top_offcanvas") ?>" class="list-group-item">Top Offcanvas</a>
+    <a href="<?= base_url("/modaloffcanvas/bottom_offcanvas") ?>" class="list-group-item">Bottom Offcanvas</a>
+    <a href="<?= base_url("/modaloffcanvas/fullscreen_offcanvas") ?>" class="list-group-item">Fullscreen Offcanvas</a>
+</div>
