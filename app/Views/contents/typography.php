7f92a070fb04f0919a854f93b58699573839e763
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:18:37 +0800

    typo link

---
+++ app/Views/contents/typography.php
@@ -0,0 +1,13 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">  <div>Typography</div></h4>
+    <a href="<?= base_url("/typography/lead") ?>" class="list-group-item">Lead</a>
+    <a href="<?= base_url("/typography/headings") ?>" class="list-group-item">Headings</a>
+    <a href="<?= base_url("/typography/display_headings") ?>" class="list-group-item">Display Headings</a>
+    <a href="<?= base_url("/typography/inline_text") ?>" class="list-group-item">Inline Text</a>
+    <a href="<?= base_url("/typography/blockquotes") ?>" class="list-group-item">Blockquotes</a>
+    <a href="<?= base_url("/typography/lists") ?>" class="list-group-item">Lists</a>
+    <a href="<?= base_url("/typography/horizontal_rule") ?>" class="list-group-item">Horizontal Rule</a>
+    <a href="<?= base_url("/typography/text_alignment") ?>" class="list-group-item">Text Alignment</a>
+    <a href="<?= base_url("/typography/text_colors") ?>" class="list-group-item">Text Colors</a>
+    <a href="<?= base_url("/typography/display_properties") ?>" class="list-group-item">Display Properties</a>
+</div>
