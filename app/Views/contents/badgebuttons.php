c7c807fca59807e7e12c70c49f52a3ad5834c44f
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:17:06 +0800

    badge link

---
+++ app/Views/contents/badgebuttons.php
@@ -0,0 +1,33 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"> Badges <div>Badge And Button</div></h4>
+    <a href="<?= base_url("/badgebuttons/badge_heading") ?>" class="list-group-item">Heading</a>
+    <a href="<?= base_url("/badgebuttons/badge_button") ?>" class="list-group-item">Button</a>
+    <a href="<?= base_url("/badgebuttons/badge_position") ?>" class="list-group-item">Positon</a>
+    <a href="<?= base_url("/badgebuttons/badge_default") ?>" class="list-group-item">Default</a>
+    <a href="<?= base_url("/badgebuttons/badge_pill") ?>" class="list-group-item">Pill</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"> Buttons <div>Badge And Button</div></h4>
+    <a href="<?= base_url("/badgebuttons/button_default") ?>" class="list-group-item">Default Buttons</a>
+    <a href="<?= base_url("/badgebuttons/button_outline") ?>" class="list-group-item">Outline Buttons</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"> Button Groups <div>Badge And Button</div></h4>
+    <a href="<?= base_url("/badgebuttons/button_group_horizontal") ?>" class="list-group-item">Horizontal Button Group</a>
+    <a href="<?= base_url("/badgebuttons/button_group_vertical") ?>" class="list-group-item">Vertical Button Group</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"> Close Buttons <div>Badge And Button</div></h4>
+    <a href="<?= base_url("/badgebuttons/close_button_default") ?>" class="list-group-item">Default Close Button</a>
+    <a href="<?= base_url("/badgebuttons/close_button_white") ?>" class="list-group-item">White Close Button</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"> Dropdowns <div>Badge And Button</div></h4>
+    <a href="<?= base_url("/badgebuttons/dropdown_default") ?>" class="list-group-item">Default Dropdown</a>
+    <a href="<?= base_url("/badgebuttons/dropdown_split") ?>" class="list-group-item">Split Dropdown</a>
+    <a href="<?= base_url("/badgebuttons/dropdown_directions") ?>" class="list-group-item">Directional Dropdowns</a>
+</div>
