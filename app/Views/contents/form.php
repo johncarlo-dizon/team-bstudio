0462294953e1abc7b4e23a2c65cd17c91c36a382
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:17:46 +0800

    form link

---
+++ app/Views/contents/form.php
@@ -0,0 +1,17 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">  
+        <div>Forms</div>
+    </h4>
+    <a href="<?= base_url("/form/basicForm") ?>" class="list-group-item">Basic Form</a>
+    <a href="<?= base_url("/form/floatingForm") ?>" class="list-group-item">Floating Form</a>
+    <a href="<?= base_url("/form/horizontalForm") ?>" class="list-group-item">Horizontal Form</a>
+    <a href="<?= base_url("/form/inlineForm") ?>" class="list-group-item">Inline Form</a>
+    <a href="<?= base_url("/form/formValidation") ?>" class="list-group-item">Form Validation</a>
+    <a href="<?= base_url("/form/fileUploadForm") ?>" class="list-group-item">File Upload Form</a>
+    <a href="<?= base_url("/form/rangeInputForm") ?>" class="list-group-item">Range Input Form</a>
+    <a href="<?= base_url("/form/disabledForm") ?>" class="list-group-item">Disabled Form</a>
+    <a href="<?= base_url("/form/checkableForm") ?>" class="list-group-item">Checkable Form</a>
+    <a href="<?= base_url("/form/contactForm") ?>" class="list-group-item">Contact Form</a>
+    <a href="<?= base_url("/form/newsletterForm") ?>" class="list-group-item">Newsletter Form</a>
+    <a href="<?= base_url("/form/addressForm") ?>" class="list-group-item">Address Form</a>
+</div>
