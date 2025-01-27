a02702b0aef2016f471a233a18564b3424c8e969
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:15:36 +0800

    navbar link

---
+++ app/Views/components/navbar.php
@@ -0,0 +1,43 @@
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Basic Navigation    <div>Navbar</div></h4>
+    <a href="<?=base_url("/navbar/nav1")?>" class="list-group-item ">Brand, Links, Dropdown & Search Form</a>
+    <a href="<?=base_url("/navbar/nav2")?>" class="list-group-item ">Brand and Background. </a>
+    <a href="<?=base_url("/navbar/nav3")?>"  class="list-group-item "> Brand and Heading   </a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Branding    <div>Navbar</div></h4>
+    <a href="<?=base_url("/navbar/nav4")?>"  class="list-group-item ">Image Logo</a>
+    <a href="<?=base_url("/navbar/nav5")?>"   class="list-group-item ">Image and text </a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Dropdown Menus    <div>Navbar</div></h4>
+    <a href="<?=base_url("/navbar/nav6")?>"   class="list-group-item ">Collapsible Menu</a>
+    <a href="<?=base_url("/navbar/nav7")?>"   class="list-group-item ">Collapsible Dropdown </a>
+    <a href="<?=base_url("/navbar/nav13")?>"  class="list-group-item ">Scrollable</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Search Forms   <div>Navbar</div></h4>
+      
+    <a href="<?=base_url("/navbar/nav8")?>"  class="list-group-item ">Search Form</a>
+    <a href="<?=base_url("/navbar/nav9")?>"  class="list-group-item ">Search Form and Brand</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Others   <div>Navbar</div></h4>
+    <a href="<?=base_url("/navbar/nav14")?>"  class="list-group-item ">Toggable Elements</a>
+    <a href="<?=base_url("/navbar/nav10")?>"  class="list-group-item ">Input Groups</a>
+    <a href="<?=base_url("/navbar/nav11")?>"  class="list-group-item ">Inline Element</a>
+    <a href="<?=base_url("/navbar/nav12")?>"  class="list-group-item ">Mix and Match</a>
+</div>
+
+ 
+
+       
+
+
+
+    
