9dcd58c68262b933e673590e3763fb52e0d2b411
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:16:00 +0800

    navtab link

---
+++ app/Views/components/navtab.php
@@ -0,0 +1,11 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"> <div>Navs and Tabs</div></h4>
+    <a href="<?= base_url("/navtab/nav_default") ?>" class="list-group-item">Default</a>
+    <a href="<?= base_url("/navtab/nav_center") ?>" class="list-group-item">Center</a>
+    <a href="<?= base_url("/navtab/nav_right") ?>" class="list-group-item">Right</a>
+    <a href="<?= base_url("/navtab/nav_vertical") ?>" class="list-group-item">Vertical</a>
+    <a href="<?= base_url("/navtab/navtabs") ?>" class="list-group-item">Tab</a>
+    <a href="<?= base_url("/navtab/navpills") ?>" class="list-group-item">Pills</a>
+    <a href="<?= base_url("/navtab/verticaltab") ?>" class="list-group-item">Vertical Tab</a>
+
+</div>
