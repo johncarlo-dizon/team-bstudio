fe583d8a2cf14cdd1761450fec99517642399257
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:14:21 +0800

    alert link

---
+++ app/Views/components/alert.php
@@ -0,0 +1,58 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Basic <div>Alert</div></h4>
+        <a href="<?= base_url("/alerts/primaryAlert") ?>" class="list-group-item">Primary Alert</a>
+        <a href="<?= base_url("/alerts/secondaryAlert") ?>" class="list-group-item">Secondary Alert</a>
+        <a href="<?= base_url("/alerts/successAlert") ?>" class="list-group-item">Success Alert</a>
+        <a href="<?= base_url("/alerts/dangerAlert") ?>" class="list-group-item">Danger Alert</a>
+        <a href="<?= base_url("/alerts/warningAlert") ?>" class="list-group-item">Warning Alert</a>
+        <a href="<?= base_url("/alerts/infoAlert") ?>" class="list-group-item">Info Alert</a>
+        <a href="<?= base_url("/alerts/lightAlert") ?>" class="list-group-item">Light Alert</a>
+        <a href="<?= base_url("/alerts/darkAlert") ?>" class="list-group-item">Dark Alert</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">With Link <div>Alert</div></h4>
+        <a href="<?= base_url("/alerts/linkAlert1") ?>" class="list-group-item">Primary with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert2") ?>" class="list-group-item">Secondary with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert3") ?>" class="list-group-item">Success with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert4") ?>" class="list-group-item">Danger with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert5") ?>" class="list-group-item">Warning with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert6") ?>" class="list-group-item">Info with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert7") ?>" class="list-group-item">Light with Link</a>
+        <a href="<?= base_url("/alerts/linkAlert8") ?>" class="list-group-item">Dark with Link</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">With Icon <div>Alert</div></h4>
+        <a href="<?= base_url("/alerts/alertWithIcon1") ?>" class="list-group-item">Primary with Icon</a>
+        <a href="<?= base_url("/alerts/alertWithIcon2") ?>" class="list-group-item">Success with Icon</a>
+        <a href="<?= base_url("/alerts/alertWithIcon3") ?>" class="list-group-item">Warning with Icon</a>
+        <a href="<?= base_url("/alerts/alertWithIcon4") ?>" class="list-group-item">Danger with Icon</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Dismissible <div>Alert</div></h4>
+        <a href="<?= base_url("/alerts/alertWithDismissButton") ?>" class="list-group-item">Dismissible Alert</a>
+</div>
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Content <div>Alert</div></h4>
+        <a href="<?= base_url("/alerts/withcontent") ?>" class="list-group-item">Simple</a>
+</div>
+
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Solid & Outline <div> Alert</div></h4>
+        <a href="<?= base_url("/alerts/alertSolid") ?>" class="list-group-item">Solid Alert</a>
+        <a href="<?= base_url("/alerts/alertOutline") ?>" class="list-group-item">Outline Alert</a>
+</div>
+
+ 
+
+
+
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Grouped <div>Alert</div></h4>
+        <a href="<?= base_url("/alerts/groupedAlerts") ?>" class="list-group-item">Grouped Alerts</a>
+</div>
