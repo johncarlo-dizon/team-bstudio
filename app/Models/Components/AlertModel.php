f76a45fe6841dede0c452f6bb8775db5c295dcab
Jomel-Guinto-Simbillo <jomelsimbillo123@gmail.com>
2025-01-27 22:45:18 +0800

    alert mdl

---
+++ app/Models/Components/AlertModel.php
@@ -0,0 +1,159 @@
+<?php
+namespace App\Models\Components;
+
+use CodeIgniter\Model;
+
+class AlertModel extends Model
+{
+    public function getAlert($alert)
+    {
+        $item = [
+    
+            'defaultAlert' => '<div class="alert alert-primary" role="alert">This is a primary alert.</div>',
+            'primaryAlert' => '<div class="alert alert-primary" role="alert">This is a primary alert.</div>',
+            'secondaryAlert' => '<div class="alert alert-secondary" role="alert">This is a secondary alert.</div>',
+            'successAlert' => '<div class="alert alert-success" role="alert">This is a success alert.</div>',
+            'dangerAlert' => '<div class="alert alert-danger" role="alert">This is a danger alert.</div>',
+            'warningAlert' => '<div class="alert alert-warning" role="alert">This is a warning alert.</div>',
+            'infoAlert' => '<div class="alert alert-info" role="alert">This is an info alert.</div>',
+            'lightAlert' => '<div class="alert alert-light" role="alert">This is a light alert.</div>',
+            'darkAlert' => '<div class="alert alert-dark" role="alert">This is a dark alert.</div>',
+            
+   
+            'linkAlert1' => '<div class="alert alert-primary" role="alert">This is a primary alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert2' => '<div class="alert alert-secondary" role="alert">This is a secondary alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert3' => '<div class="alert alert-success" role="alert">This is a success alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert4' => '<div class="alert alert-danger" role="alert">This is a danger alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert5' => '<div class="alert alert-warning" role="alert">This is a warning alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert6' => '<div class="alert alert-info" role="alert">This is a info alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert7' => '<div class="alert alert-light" role="alert">This is a light alert with <a href="#" class="alert-link">an example link</a>.</div>',
+            'linkAlert8' => '<div class="alert alert-dark" role="alert">This is a dark alert with <a href="#" class="alert-link">an example link</a>.</div>',
+
+          
+            'alertWithIcon1' => 
+            '
+                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                  </symbol>
+                  <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                  </symbol>
+                  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </symbol>
+                </svg>
+
+                <div class="alert alert-primary d-flex align-items-center" role="alert">
+                  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
+                  <div>
+                    An example alert with an icon
+                  </div>
+                </div>
+
+          ',
+
+          'alertWithIcon2' => 
+          '
+                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                  </symbol>
+                  <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                  </symbol>
+                  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </symbol>
+                </svg>
+
+
+                <div class="alert alert-success d-flex align-items-center" role="alert">
+                  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                  <div>
+                    An example success alert with an icon
+                  </div>
+                </div>
+
+          ',
+
+          'alertWithIcon3' => 
+          '
+                  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                    <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </symbol>
+                    <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                    </symbol>
+                    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </symbol>
+                  </svg>
+
+                  <div class="alert alert-warning d-flex align-items-center" role="alert">
+                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                    <div>
+                      An example warning alert with an icon
+                    </div>
+                  </div>
+
+          ',
+
+          'alertWithIcon4' => 
+          '
+                  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                    <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </symbol>
+                    <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                    </symbol>
+                    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </symbol>
+                  </svg>
+
+
+                  <div class="alert alert-danger d-flex align-items-center" role="alert">
+                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                    <div>
+                      An example danger alert with an icon
+                    </div>
+                  </div>
+        ',
+            
+
+            'alertWithDismissButton' => '
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> This is a dismissible danger alert.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>',
+
+
+
+            
+        
+            'alertSolid' => '<div class="alert alert-primary" role="alert" style="background-color: #007bff; color: #fff;">This is a solid alert.</div>',
+            'alertOutline' => '<div class="alert alert-outline-primary" role="alert" style="border: 1px solid #007bff;">This is an outline alert.</div>',
+            
+   
+       
+            'groupedAlerts' => '
+                <div class="alert alert-success" role="alert">Success message 1</div>
+                <div class="alert alert-info" role="alert">Info message 2</div>
+                <div class="alert alert-warning" role="alert">Warning message 3</div>',
+
+            'withcontent' => '
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                <hr>
+                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+              </div>
+            ',
+        ];
+
+        return isset($item[$alert]) ? $item[$alert] : null;
+    }
+}
