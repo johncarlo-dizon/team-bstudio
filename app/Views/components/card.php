efebd439ba8ae163ff24f79a4cd2bdee607cbf6a
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:14:34 +0800

    card link

---
+++ app/Views/components/card.php
@@ -0,0 +1,80 @@
+ 
+
+<div class="card-category">
+    
+    <h4 class="d-flex justify-content-between">Background & Color   <div>Card</div></h4>
+    <a href="<?=base_url("/card/card19")?>" class="list-group-item">Primary</a>
+    <a href="<?=base_url("/card/card20")?>" class="list-group-item">Secondary</a>
+    <a href="<?=base_url("/card/card21")?>" class="list-group-item">Success</a>
+    <a href="<?=base_url("/card/card22")?>" class="list-group-item">Danger</a>
+    <a href="<?=base_url("/card/card23")?>" class="list-group-item">Warning</a>
+    <a href="<?=base_url("/card/card24")?>" class="list-group-item">Info</a>
+    <a href="<?=base_url("/card/card25")?>" class="list-group-item">Light</a>
+    <a href="<?=base_url("/card/card26")?>" class="list-group-item">Dark</a>
+</div>
+
+
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Border   <div>Card</div></h4>
+    <a href="<?=base_url("/card/card27")?>" class="list-group-item">Primary</a>
+    <a href="<?=base_url("/card/card28")?>" class="list-group-item">Secondary</a>
+    <a href="<?=base_url("/card/card29")?>" class="list-group-item">Success</a>
+    <a href="<?=base_url("/card/card30")?>" class="list-group-item">Danger</a>
+    <a href="<?=base_url("/card/card31")?>" class="list-group-item">Warning</a>
+    <a href="<?=base_url("/card/card32")?>" class="list-group-item">Info</a>
+    <a href="<?=base_url("/card/card33")?>" class="list-group-item">Light</a>
+    <a href="<?=base_url("/card/card34")?>" class="list-group-item">Dark</a>
+</div>
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Images   <div>Card</div></h4>
+    <a href="<?=base_url("/card/card4")?>" class="list-group-item">Card Image Top</a>
+    <a href="<?=base_url("/card/card15")?>" class="list-group-item">Image Top</a>
+    <a href="<?=base_url("/card/card16")?>" class="list-group-item">Image Bottom</a>
+    <a href="<?=base_url("/card/card17")?>" class="list-group-item">Image Overlay</a>
+    <a href="<?=base_url("/card/card18")?>" class="list-group-item">Image Left</a>
+</div>
+<div class="card-category">
+    <h4 class="d-flex justify-content-between">Layouts  <div>Card</div></h4>
+    <a href="<?=base_url("/card/card35")?>" class="list-group-item">Basic</a>
+    <a href="<?=base_url("/card/card36")?>" class="list-group-item">Basic w/ Footer</a>
+    <a href="<?=base_url("/card/card38")?>" class="list-group-item">Wrap</a>
+    <a href="<?=base_url("/card/card37")?>" class="list-group-item">Equal Width</a>
+    <a href="<?=base_url("/card/card39")?>" class="list-group-item">Equal Height</a>
+    <a href="<?=base_url("/card/card39")?>" class="list-group-item">Equal Height w/ Footer</a>
+</div>
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Layout and Structure Variants   <div>Card</div></h4>
+    <a href="<?=base_url("/card/card8")?>" class="list-group-item">Kitchen Sink</a>
+    <a href="<?=base_url("/card/card9")?>" class="list-group-item">Header & Footer</a>
+    <a href="<?=base_url("/card/card12")?>" class="list-group-item">Grid Markup</a>
+</div>
+
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Navigation   <div>Card</div></h4>
+ 
+    <a href="<?=base_url("/card/card13")?>" class="list-group-item">Nav Tabs</a>
+    <a href="<?=base_url("/card/card14")?>" class="list-group-item">Nav Pills</a>
+</div>
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Basic    <div>Card</div></h4>
+ 
+    <a href="<?=base_url("/card/card1")?>" class="list-group-item">Basic Card</a>
+    <a href="<?=base_url("/card/card2")?>" class="list-group-item">Card Body</a>
+</div>
+
+<div class="card-category">
+<h4 class="d-flex justify-content-between">Titles and Text   <div>Card</div></h4>
+    <a href="<?=base_url("/card/card3")?>" class="list-group-item">Title, Text and Links</a>
+    <a href="<?=base_url("/card/card10")?>" class="list-group-item">Quote Card</a>
+    <a href="<?=base_url("/card/card11")?>" class="list-group-item">Text Center</a>
+</div>
+<div class="card-category">
+<h4 class="d-flex justify-content-between">List   <div>Card</div></h4>
+ 
+    <a href="<?=base_url("/card/card5")?>" class="list-group-item">List Card</a>
+    <a href="<?=base_url("/card/card6")?>" class="list-group-item">List Card with Header</a>
+    <a href="<?=base_url("/card/card7")?>" class="list-group-item">List Card with Footer</a>
+</div>
+
+
+ 
