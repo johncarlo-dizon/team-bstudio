4147bc743b62314072aa4a45cc1c0526eb71f746
Dizon-Joel <joeldizon111111@gmail.com>
2025-01-27 21:21:00 +0800

    modeloffcanvas

---
+++ app/Controllers/Components/ModalOffcanvasCtrl.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers\Components;
+use App\Controllers\BaseController;
+use App\Controllers\Dashboard;
+use App\Models\Components\ModalCanvasModel;
+
+class ModalOffcanvasCtrl  extends BaseController
+{
+    public $dashboard;
+    public $session;
+    public $model;
+
+    public function __construct(){
+      $this->dashboard = new Dashboard();
+      $this->session = session();
+      $this->model = new ModalCanvasModel();
+    }
+
+    public function display($param1 = null)
+    {
+   
+        $param = $param1;
+  
+    
+        if ($param) {
+            $this->session->set("param", $param);
+            $paramdata = $this->model->getdata($param);
+            $this->session->set("paramdata", $paramdata);
+    
+          
+            $this->dashboard ->assignToNextContainer($paramdata);
+        }
+    
+        return redirect()->to(base_url() . "dashboard");
+    }
+}
