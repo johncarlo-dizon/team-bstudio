93c714f6ff9c6d1dd489a9e24052b1003c5c1367
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:14:53 +0800

    carousel link

---
+++ app/Views/components/carousel.php
@@ -0,0 +1,14 @@
+
+<div class="card-category">
+    
+    <h4 class="d-flex justify-content-between">  <div>Carousel</div></h4>
+          <a href="<?=base_url("/carousel/car1")?>" class="list-group-item ">Slides Only</a>
+          <a href="<?=base_url("/carousel/car2")?>"  class="list-group-item ">With Controls</a>
+          <a href="<?=base_url("/carousel/car3")?>" class="list-group-item ">With Indicators </a>
+          <a href="<?=base_url("/carousel/car4")?>" class="list-group-item ">With Captions</a>
+          <a href="<?=base_url("/carousel/car5")?>" class="list-group-item ">Crossfade</a>
+          <a href="<?=base_url("/carousel/car6")?>" class="list-group-item ">Individual Item Interval</a>
+          <a href="<?=base_url("/carousel/car7")?>" class="list-group-item ">Disable Touch Swiping</a>
+          <a href="<?=base_url("/carousel/car7")?>" class="list-group-item ">Dark Variant</a>
+</div>
+
