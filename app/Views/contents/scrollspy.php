6f32c274a1db243517f2df0ba9968dcbf4b4118b
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-01-27 23:18:05 +0800

    scroll spy link

---
+++ app/Views/contents/scrollspy.php
@@ -0,0 +1,8 @@
+<div class="card-category">
+    <h4 class="d-flex justify-content-between"><div>Scrollspy</div></h4>
+    <a href="<?= base_url(relativePath: "/scrollspy/navbar") ?>" class="list-group-item">Navbar Scroll</a>
+    <a href="<?= base_url("/scrollspy/nestednav") ?>" class="list-group-item">Nested Nav Scroll</a>
+
+    <a href="<?= base_url("/scrollspy/listgroup") ?>" class="list-group-item">List Scroll</a>
+ 
+</div>
