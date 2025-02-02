af784e9a817885791ebd91145edcd4c764ce0c13
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-02-03 02:22:56 +0800

    modal

--- app/Views/innerlayout/base.php
+++ app/Views/innerlayout/base.php
@@ -690,9 +690,9 @@
 
 
     
-    <div class="modal fade" id="tempform" tabindex="-1" aria-labelledby="modallabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
+    <div class="modal fade " id="tempform" tabindex="-1" aria-labelledby="modallabel" aria-hidden="true">
+          <div class="modal-dialog ">
+            <div class="modal-content inner">
               <div class="modal-header">
                 <h5 class="modal-title" id="modallabel">Default Modal</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -705,8 +705,8 @@
 
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit">Submit</button>
+                <button type="button" class="btn btn-action" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-action">Submit</button>
                 <?= form_close() ?>
               </div>
             </div>
