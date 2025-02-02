dbebb9eaeee84e1761501b4c568dded844d488cc
Aaronjireh <aaronavendano09@gmail.com>
2025-02-02 13:10:57 +0800

    outer logo title

--- app/Views/outerlayout/base.php
+++ app/Views/outerlayout/base.php
@@ -4,8 +4,8 @@
   <title>Codecanvas</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" class="rounded-5" type="image/png" sizes="32x32" href="<?=base_url()?>/uploads/bstudio.webp">
-  <link rel="icon" class="rounded-5" type="image/png" sizes="16x16" href="<?=base_url()?>/uploads/bstudio.webp">
+  <link rel="icon" class="rounded-5" type="image/png" sizes="32x32" href="<?=base_url()?>/uploads/CB.png">
+  <link rel="icon" class="rounded-5" type="image/png" sizes="16x16" href="<?=base_url()?>/uploads/CB.png">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
