63ecdf873f106e48059c73963567c3d39c5ef493
Aaronjireh <aaronavendano09@gmail.com>
2025-02-02 14:07:47 +0800

    register image

--- app/Controllers/OuterController/Register.php
+++ app/Controllers/OuterController/Register.php
@@ -62,7 +62,8 @@ class Register extends BaseController
                   <a href="' . base_url() . 'activate/' . $uniid . '" target="_blank">Activate My Account</a><br><br>
                   If you didn\'t request this, please ignore this email.<br><br>
                   Best regards,<br>
-                  CODE CANVAS';
+                  CODE CANVAS<br><br>
+                  <img src="' . base_url() . '/uploads/codebro.jpeg" alt="Logo" style="max-width: 200px; height: auto;">';
                   
 
                   $this->email->setTo($to);
