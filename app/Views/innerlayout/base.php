aa90d36d607a58e2d0361ac8a7280993d489042f
Dizon-Joel <joeldizon111111@gmail.com>
2025-02-03 02:07:37 +0800

    save modal

--- app/Views/innerlayout/base.php
+++ app/Views/innerlayout/base.php
@@ -681,6 +681,33 @@
     </div>
 
 
+
+
+
+    
+    <div class="modal fade" id="tempform" tabindex="-1" aria-labelledby="modallabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modallabel">Default Modal</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+              <?= form_open(base_url().'savecode', ['id' => 'codeForm']) ?> 
+                <input type="hidden" name="tempemail" value="<?= ucfirst($userdata->email) ?>">
+                <input type="text" class="form-control" name="tempname" placeholder="Project Name">
+                <textarea id="hiddenInput" name="codeContent" style="display: none;"></textarea>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit">Submit</button>
+                <?= form_close() ?>
+              </div>
+            </div>
+          </div>
+        </div>
+
   
 
 
